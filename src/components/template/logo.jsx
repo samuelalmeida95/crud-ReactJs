eaa19e5f6f02a58a304e404ee145f4fc6743c8bd
samuelalmeida95 <samuelalmeida95>
2021-02-11 20:47:29 -0300

    alterando logo do projeto

--- src/components/template/logo.jsx
+++ src/components/template/logo.jsx
@@ -1,12 +1,12 @@
 import './logo.css'
 import React from 'react'
-/* import logo from '../../assets/images/logo.png' */
+import logo from '../../assets/images/logo.png'
 import {Link} from 'react-router-dom'
 
 const Logo = props => 
     <aside className="logo">
         <Link to="/" className="logo">
-          {/*   <img src={logo} alt="logo"/> */}
+            <img src={logo} alt="logo"/>
         </Link>
     </aside>
 
